def163e1882cf82de3509ce27b7fd32af61435ad
Riko <riko.fluchel@gmail.com>
2025-05-16 14:45:09 -0400

    render mindful stats

--- src/components/MindfulStats.js
+++ src/components/MindfulStats.js
@@ -1,9 +1,39 @@
+import { useOutletContext } from "react-router-dom";
 
 function MindfulStats(){
 
+    const { entries } = useOutletContext();
+
+    const entryCount = entries.length
+
+    const totalMindfulMinutes = entries.reduce((acc, entry) => {
+       return acc += Number(entry.duration);
+    }, 0);
+
+    const moodCounts = entries.reduce((acc, entry) => {
+        if (!acc[entry.mood]){
+            acc[entry.mood] = 1
+        }
+        else {
+            acc[entry.mood] += 1
+        };
+        return acc;
+    },{});
+
+    console.log("mood counts", moodCounts)
+
     return (
-        <div>
-            <p>coming soon!</p>
+        <div className="stats">
+            <p>{entryCount} entries</p>
+           <p>Total mindful minutes: {totalMindfulMinutes}</p>
+            <p>You felt...</p>
+           <ul>
+                <li>great {moodCounts.great} times</li>
+                <li>good {moodCounts.good} times</li>
+                <li>meh {moodCounts.meh} times</li>
+                <li>bad {moodCounts.bad} times</li>
+                <li>awful {moodCounts.awful} times</li>
+           </ul>
         </div>
     )
 };
